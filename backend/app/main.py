54c23c228f25f873553cb48f9f4b85f55b00a2c1
Khalid200704 <abdullah20khalid20@gmail.com>
2026-06-01 16:09:17 +0700

    fix: allow hf.space host

--- backend/app/main.py
+++ backend/app/main.py
@@ -56,7 +56,7 @@ app.add_middleware(
 # Trusted hosts
 app.add_middleware(
     TrustedHostMiddleware,
-    allowed_hosts=["localhost", "127.0.0.1", "10.10.10.76", "192.168.110.250", "*.deschain.id", "*.vercel.app", "*.railway.app", "*.up.railway.app"],
+    allowed_hosts=["localhost", "127.0.0.1", "10.10.10.76", "192.168.110.250", "*.deschain.id", "*.vercel.app", "*.railway.app", "*.up.railway.app", "*.hf.space"],
 )
 
 
